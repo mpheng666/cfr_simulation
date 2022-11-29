7517f7efa84f95834badb5d86c51eb729f570f54
mpheng666 <mpheng96@gmail.com>
2022-11-30 07:09:24 +0800

    Fix client fixed length read

--- cfr_socket_comm/src/cfr_socket_comm_node.cpp
+++ cfr_socket_comm/src/cfr_socket_comm_node.cpp
@@ -1,7 +1,10 @@
 #include "cfr_socket_comm/cfr_socket_server.hpp"
 
-int main(int argc, char ** argv)
+int main(int argc, char** argv)
 {
+    // cfr_socket_comm::Server srv;
+    // srv.listen(10000);
+    // srv.run();
     boost::asio::io_context io_context;
 
     cfr_socket_comm::CFRSocketServer cfr_socket_server(io_context, 10000);

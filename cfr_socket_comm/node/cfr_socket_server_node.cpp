115a5bd49a6a00b5e7f5f8e3c39552cfbb8b6cf9
mpheng666 <mpheng96@gmail.com>
2022-12-09 11:09:49 +0800

    Fix server parse bug

--- cfr_socket_comm/node/cfr_socket_server_node.cpp
+++ cfr_socket_comm/node/cfr_socket_server_node.cpp
@@ -5,8 +5,8 @@ int main(int argc, char** argv)
     static constexpr int DEFAULT_PORT {10000};
     cfr_socket_comm::CFRSocketServer cfr_socket_server;
     if (argc > 1) {
-        cfr_socket_server.listen(std::atoi(argv[0]));
-        std::cout << "Running localhost server, listening to port " << std::atoi(argv[0]) << "\n";
+        cfr_socket_server.listen(std::atoi(argv[1]));
+        std::cout << "Running localhost server, listening to port " << std::atoi(argv[1]) << "\n";
     }
     else {
         cfr_socket_server.listen(DEFAULT_PORT);

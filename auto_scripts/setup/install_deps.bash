60fdab80e812fb447340c0bccc877f14e0ba61d9
mpheng666 <mpheng96@gmail.com>
2022-12-22 01:31:48 +0700

    Fix setup script

--- auto_scripts/setup/install_deps.bash
+++ auto_scripts/setup/install_deps.bash
@@ -1,5 +1,4 @@
 sudo apt install ros-foxy-xacro
-sudo apt install ros-foxy-gazebo-ros
-sudo apt install ros-foxy-gazebo-ros2-control
+sudo apt install ros-foxy-gazebo-*
 sudo apt install ros-foxy-ros2-control
 sudo apt install ros-foxy-ros2-controllers

cb8400ab4aa5ae806bb988bf772d84c3d8ed778b
mpheng666 <mpheng96@gmail.com>
2022-12-31 15:37:56 +0800

    Add services to message handler

--- cfr_socket_comm/src/message_handler.cpp
+++ cfr_socket_comm/src/message_handler.cpp
@@ -2,6 +2,44 @@
 
 namespace cfr_socket_comm {
 
-    std::string MessageHandler::handleMessage(const std::string& msg) { return "OK"; }
+    std::string MessageHandler::handleMessage(const std::string& msg)
+    {
+        if (msg == "STATUS") {
+            cfr_sm_client::CFRSMClient cfr_state_machine_client_("cfr_sm_client_node");
+            auto a_service = std::async(&cfr_sm_client::CFRSMClient::callCFRService,
+                                        &cfr_state_machine_client_, msg);
+        }
+        if (msg == "FB") {
+            cfr_sm_client::CFRSMClient cfr_state_machine_client_("cfr_sm_client_node");
+            auto a_service = std::async(&cfr_sm_client::CFRSMClient::callCFRService,
+                                        &cfr_state_machine_client_, msg);
+        }
+        if (msg == "INIT") {
+            cfr_sm_client::CFRSMClient cfr_state_machine_client_("cfr_sm_client_node");
+            auto a_service = std::async(&cfr_sm_client::CFRSMClient::callCFRService,
+                                        &cfr_state_machine_client_, msg);
+        }
+        if (msg == "START") {
+            cfr_sm_client::CFRSMClient cfr_state_machine_client_("cfr_sm_client_node");
+            auto a_service = std::async(&cfr_sm_client::CFRSMClient::callCFRService,
+                                        &cfr_state_machine_client_, msg);
+        }
+        if (msg == "STOP") {
+            cfr_sm_client::CFRSMClient cfr_state_machine_client_("cfr_sm_client_node");
+            auto a_service = std::async(&cfr_sm_client::CFRSMClient::callCFRService,
+                                        &cfr_state_machine_client_, msg);
+        }
+        if (msg == "RESET") {
+            cfr_sm_client::CFRSMClient cfr_state_machine_client_("cfr_sm_client_node");
+            auto a_service = std::async(&cfr_sm_client::CFRSMClient::callCFRService,
+                                        &cfr_state_machine_client_, msg);
+        }
+        if (msg == "CTRL") {
+        }
+        else {
+            std::cout << "Discard invalid message \n";
+        }
+        
+    }
 
 } // namespace cfr_socket_comm

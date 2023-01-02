b212a03d6d21e36ff10e7cb7ab16d43f87d0b968
mpheng666 <mpheng96@gmail.com>
2023-01-02 20:04:22 +0800

    Add server asyn accept multiple clients

--- cfr_feedback_server/include/cfr_feedback_server/cfr_feedback_server.hpp
+++ cfr_feedback_server/include/cfr_feedback_server/cfr_feedback_server.hpp
@@ -2,21 +2,77 @@
 #define CFR_FEEDBACK_SERVER_HPP_
 
 #include <boost/asio.hpp>
+#include <boost/bind/bind.hpp>
+#include <boost/enable_shared_from_this.hpp>
+#include <boost/shared_ptr.hpp>
+#include <ctime>
 #include <iostream>
 #include <string>
 
 using boost::asio::ip::tcp;
 
 namespace cfr_feedback_server {
+
+    class tcp_connection : public boost::enable_shared_from_this<tcp_connection> {
+    public:
+        typedef boost::shared_ptr<tcp_connection> pointer;
+
+        static pointer create(boost::asio::io_context& io_context)
+        {
+            return pointer(new tcp_connection(io_context));
+        }
+
+        tcp::socket& socket() { return socket_; }
+
+        void start()
+        {
+            boost::asio::async_write(
+            socket_, boost::asio::buffer("Hello from CFR feedback server \n"),
+            boost::bind(&tcp_connection::handle_write, shared_from_this(),
+                        boost::asio::placeholders::error,
+                        boost::asio::placeholders::bytes_transferred));
+        }
+
+        void write(const std::string& msg)
+        {
+            const std::string delimiter{"\n"};
+
+            boost::asio::async_write(
+            socket_, boost::asio::buffer(msg + delimiter),
+            boost::bind(&tcp_connection::handle_write, shared_from_this(),
+                        boost::asio::placeholders::error,
+                        boost::asio::placeholders::bytes_transferred));
+        }
+
+    private:
+        tcp_connection(boost::asio::io_context& io_context)
+            : socket_(io_context)
+        {
+        }
+
+        void handle_write(const boost::system::error_code& /* error */,
+                          size_t /* bytes_transferred */)
+        {
+            // std::cout << "Write bytes: " << bytes_transferred << "\n";
+        }
+
+        tcp::socket socket_;
+        std::string message_;
+    };
     class CFRFeedBackServer {
     public:
         CFRFeedBackServer(const int port);
+        void run();
         bool write(const std::string& message);
 
     private:
         boost::asio::io_context io_context_;
         tcp::acceptor acceptor_;
-        tcp::socket socket_;
+        std::vector<tcp_connection::pointer> connections_;
+
+        void start_accept();
+        void handle_accept(tcp_connection::pointer new_connection,
+                           const boost::system::error_code& error);
     };
 } // namespace cfr_feedback_server
 
--- cfr_feedback_server/src/cfr_feedback_server.cpp
+++ cfr_feedback_server/src/cfr_feedback_server.cpp
@@ -1,31 +1,46 @@
 #include "cfr_feedback_server/cfr_feedback_server.hpp"
 
 namespace cfr_feedback_server {
+
     CFRFeedBackServer::CFRFeedBackServer(const int port)
         : acceptor_(io_context_, tcp::endpoint(tcp::v4(), port))
-        , socket_(io_context_)
     {
-        try {
-            acceptor_.accept(socket_);
-        }
-        catch (const std::exception& e) {
-            std::cerr << e.what() << '\n';
-        }
+        std::cout << "Started feedback server at port " << port << " \n";
+        start_accept();
     }
+
+    void CFRFeedBackServer::run() { io_context_.run(); }
 
     bool CFRFeedBackServer::write(const std::string& message)
     {
-        size_t written_size{};
-        try {
-            boost::system::error_code ignored_error;
-            written_size =
-            boost::asio::write(socket_, boost::asio::buffer(message), ignored_error);
-        }
-        catch (const std::exception& e) {
-            std::cerr << e.what() << '\n';
+        for(const auto& connection : connections_)
+        {
+            connection->write(message);
         }
 
-        return message.size() == written_size;
+        return true;
+    }
+
+    void CFRFeedBackServer::start_accept()
+    {
+        tcp_connection::pointer new_connection = tcp_connection::create(io_context_);
+
+        acceptor_.async_accept(new_connection->socket(),
+                               boost::bind(&CFRFeedBackServer::handle_accept, this,
+                                           new_connection,
+                                           boost::asio::placeholders::error));
+    }
+
+    void CFRFeedBackServer::handle_accept(tcp_connection::pointer new_connection,
+                                        const boost::system::error_code& error)
+    {
+        if (!error) {
+            new_connection->start();
+            connections_.push_back(new_connection);
+            std::cout << "Push back new connection! \n";
+        }
+
+        start_accept();
     }
 
 } // namespace cfr_feedback_server

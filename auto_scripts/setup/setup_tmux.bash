c5d4278d072fec69813e8358a7b38b3cbe4b33a4
mpheng666 <mpheng96@gmail.com>
2023-03-02 00:35:53 +0800

    Update tmuxinator setup script and cfr_rc.yml

--- auto_scripts/setup/setup_tmux.bash
+++ auto_scripts/setup/setup_tmux.bash
@@ -1,3 +1,17 @@
 #!/bin/bash
 sudo apt install tmuxinator -y
-ln -s ../tmux/tmuxinator/* ~/.config/tmuxinator
+DIR=~/.config/tmuxinator
+if [ -d "$DIR" ];
+then
+    # sudo rm ~/.config/tmuxinator/*
+    echo "$DIR directory exists."
+else
+    echo "$DIR directory does not exist, creating new directory"
+    mkdir "$DIR"
+fi
+# FILES="/home/mpheng/workspace/cfr_ws/src/cfr_simulation/auto_scripts/tmux/tmuxinator/*"
+FILES=~/workspace/cfr_ws/src/auto_scripts/tmux/tmuxinator/*
+for f in "$FILES"
+do
+    ln -s $f ~/.config/tmuxinator/
+done

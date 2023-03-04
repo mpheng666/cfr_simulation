31f3f166ad6bb05a8cbc2eb7768729efdac21272
mpheng666 <mpheng96@gmail.com>
2023-03-04 10:54:52 +0800

    Compile mpc

--- cfr_mpc/include/cfr_mpc/cfr_mpc.hpp
+++ cfr_mpc/include/cfr_mpc/cfr_mpc.hpp
@@ -2,17 +2,16 @@
 #define _CFR_MPC_CFR_MPC_HPP_
 
 // platform type definition
-#include "rtwtypes.h"
+#include "lib/rtwtypes.h"
 
 // platform compiler definition
 #include "lib/mpcmoveCodeGeneration_spec.h"
 
-// mpc type definition
-#include "lib/mpcmoveCodeGeneration_types.h"
-
 // optimization function
 #include "lib/qpkwik.h"
 
+// actual mpc
+#include "lib/mpcmoveCodeGeneration.h"
 
 #include "rclcpp/rclcpp.hpp"
 
@@ -24,15 +23,51 @@
 using namespace std::chrono_literals;
 using std::placeholders::_1;
 
-namespace cfr_mpc {
+// Type Definitions
+// struct struct10_T {
+//     double Uopt[33];
+//     double Yopt[33];
+//     double Xopt[66];
+//     double Topt[11];
+//     double Slack;
+//     double Iterations;
+//     double Cost;
+// };
 
+// struct struct4_T {
+//     double Plant[3];
+//     double Disturbance[3];
+//     double LastMove[3];
+//     double Covariance[36];
+//     boolean_T iA[72];
+// };
+
+// struct struct6_T {
+//     double ym[3];
+//     double ref[3];
+// };
+
+// struct struct5_T {
+//     struct6_T signals;
+// };
+
+namespace cfr_mpc {
     class CFRMPC : public rclcpp::Node {
     public:
         CFRMPC();
         ~CFRMPC();
 
     private:
-        rclcpp::WallTimer mpc_compute_cb_timer_;
+        rclcpp::TimerBase::SharedPtr mpc_compute_cb_timer_;
+
+        void argInit_3x1_real_T(double result[3]);
+        void argInit_6x6_real_T(double result[36]);
+        void argInit_72x1_boolean_T(boolean_T result[72]);
+        boolean_T argInit_boolean_T();
+        double argInit_real_T();
+        void argInit_struct4_T(struct4_T* result);
+        struct5_T argInit_struct5_T();
+        struct6_T argInit_struct6_T();
 
         void MPCComputeCb();
     };
--- cfr_mpc/src/cfr_mpc.cpp
+++ cfr_mpc/src/cfr_mpc.cpp
@@ -4,11 +4,14 @@ namespace cfr_mpc {
     CFRMPC::CFRMPC()
         : Node("cfr_mpc")
         , mpc_compute_cb_timer_(
-          this->create_wall_timer(100ms, std::bind(&CFRMPC::MPCGenerationCb, this)))
+          this->create_wall_timer(100ms, std::bind(&CFRMPC::MPCComputeCb, this)))
     {
     }
 
-    CFRMPC::~CFRMPC() { mpcmoveCodeGeneration_terminate(); }
+    CFRMPC::~CFRMPC()
+    {
+        // mpcmoveCodeGeneration_terminate();
+    }
 
     void CFRMPC::MPCComputeCb()
     {
@@ -20,13 +23,89 @@ namespace cfr_mpc {
         // Initialize function input argument 'statedata'.
         // Initialize function input argument 'onlinedata'.
         // Call the entry-point 'mpcmoveCodeGeneration'.
-        argInit_struct4_T(&statedata);
-        r = argInit_struct5_T();
-        r.signals.ref[2] = 0.5;
+        argInit_struct4_T(&statedata); // set 0
+        r = argInit_struct5_T();       // set 0
+        r.signals.ref[2] = 0.5;        // set ref to 0.5
         printf("output r is %f, %f %f\n", r.signals.ref[0], r.signals.ref[1],
                r.signals.ref[2]);
+        // statedata = plant[3], disturbance[3], lastmove[3], covariance[36], ia[72]
+        // r = signal.ym[3], signal.ref[3]
+        // u = double[3]
+        // Info = Uopt[33], Yopt[33], Xopt[66], Topt[11], Slack, Iterations, Cost;
         coder::mpcmoveCodeGeneration(&statedata, &r, u, &Info);
         printf("output u is %f, %f %f\n", u[0], u[1], u[2]);
+    }
+
+    void CFRMPC::argInit_3x1_real_T(double result[3])
+    {
+        // Loop over the array to initialize each element.
+        for (int idx0{0}; idx0 < 3; idx0++) {
+            // Set the value of the array element.
+            // Change this value to the value that the application requires.
+            result[idx0] = argInit_real_T();
+        }
+    }
+
+    void CFRMPC::argInit_6x6_real_T(double result[36])
+    {
+        // Loop over the array to initialize each element.
+        for (int idx0{0}; idx0 < 6; idx0++) {
+            for (int idx1{0}; idx1 < 6; idx1++) {
+                // Set the value of the array element.
+                // Change this value to the value that the application requires.
+                result[idx0 + 6 * idx1] = argInit_real_T();
+            }
+        }
+    }
+
+    void CFRMPC::argInit_72x1_boolean_T(boolean_T result[72])
+    {
+        // Loop over the array to initialize each element.
+        for (int idx0{0}; idx0 < 72; idx0++) {
+            // Set the value of the array element.
+            // Change this value to the value that the application requires.
+            result[idx0] = argInit_boolean_T();
+        }
+    }
+
+    boolean_T CFRMPC::argInit_boolean_T() { return false; }
+
+    double CFRMPC::argInit_real_T() { return 0.0; }
+
+    void CFRMPC::argInit_struct4_T(struct4_T* result)
+    {
+        argInit_3x1_real_T(result->Plant);      // set all fields to double zero
+        argInit_6x6_real_T(result->Covariance); // set all fields to double zero
+        argInit_72x1_boolean_T(result->iA);     // set all fields to false
+        // set all disturbance and lastmove to plant
+        result->Disturbance[0] = result->Plant[0];
+        result->Disturbance[1] = result->Plant[1];
+        result->Disturbance[2] = result->Plant[2];
+        result->LastMove[0] = result->Plant[0];
+        result->LastMove[1] = result->Plant[1];
+        result->LastMove[2] = result->Plant[2];
+    }
+
+    struct5_T CFRMPC::argInit_struct5_T()
+    {
+        struct5_T result;
+        // Set the value of each structure field.
+        // Change this value to the value that the application requires.
+        result.signals = argInit_struct6_T();
+
+        return result;
+    }
+
+    struct6_T CFRMPC::argInit_struct6_T()
+    {
+        struct6_T result;
+        // Set the value of each structure field.
+        // Change this value to the value that the application requires.
+        // result.ref[1]=1;
+        // printf("result is %f, %f %f\n",result.ref[0],result.ref[1],result.ref[2]);
+        argInit_3x1_real_T(result.ym);
+        argInit_3x1_real_T(result.ref);
+        return result;
     }
 
 } // namespace cfr_mpc

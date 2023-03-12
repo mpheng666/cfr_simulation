ab4b28485b647848ea482beff967945c2a084812
mpheng666 <mpheng96@gmail.com>
2023-03-13 07:19:25 +0800

    Update tmux for path tracker

--- auto_scripts/setup/setup_tmux.bash
+++ auto_scripts/setup/setup_tmux.bash
@@ -9,8 +9,8 @@ else
     echo "$DIR directory does not exist, creating new directory"
     mkdir "$DIR"
 fi
-# FILES=~/workspace/cfr_ws/src/cfr_simulation/auto_scripts/tmux/tmuxinator/*
-FILES=~/workspace/cfr_ws/src/auto_scripts/tmux/tmuxinator/*
+FILES=~/workspace/cfr_ws/src/cfr_simulation/auto_scripts/tmux/tmuxinator/*
+# FILES=~/workspace/cfr_ws/src/auto_scripts/tmux/tmuxinator/*
 for f in "$FILES"
 do
     ln -s $f ~/.config/tmuxinator/
